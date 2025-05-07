262d057a6cf8c3e06c7c9504ff1ea7a03c22fe81
Naveenkumar S E <98598772+naveenkumarse@users.noreply.github.com>
2025-05-07 19:56:56 +0530

    Update MainActivity.kt

--- Android-examples/listview/MainActivity.kt
+++ Android-examples/listview/MainActivity.kt
@@ -2,7 +2,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_main)
 
         val listView = findViewById<ListView>(R.id.myListView)
 
